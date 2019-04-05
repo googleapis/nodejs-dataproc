0a5e91f9ae70ab8a62242a87353581f20be2ca9f
Jonathan Lui <jonathanlui@google.com>
2019-04-04 17:06:51 -0700

    fixit

--- samples/system-test/quickstart.test.js
+++ samples/system-test/quickstart.test.js
@@ -14,17 +14,9 @@
  */
 
 const {assert} = require('chai');
-<<<<<<< HEAD
 const cp = require('child_process');
 
-const execSync = (cmd) => cp.execSync(cmd, {encoding: 'utf-8'});
-
-const execSync = (cmd) => cp.execSync(cmd, {encoding: 'utf-8'});
-=======
-const cp = require('child_process');
->>>>>>> master
-
-const execSync = (cmd) => cp.execSync(cmd, {encoding: 'utf-8'});
+const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
 describe('dataproc samples', () => {
   it('should run the quickstart', async () => {
